8e8e46019a380041685c0a6e68a9733185cd178d
thirty8zero <danny.palamountain@gmail.com>
2017-11-13 16:21:38 +1030

    Doors work

---
+++ VR Base Test Project/Assets/Scripts/DoorOpener.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpener : MonoBehaviour {
+
+    public GameObject door;
+    public Vector3 rotateOpen, rotateClose;
+    bool isOpen = false;
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Controller" && Input.GetButtonDown("Fire1"))
+        {
+            if (isOpen == true)
+            {
+                LeanTween.rotate(door, rotateClose, 1f);
+                isOpen = !isOpen;
+            }
+            else if (isOpen == false)
+            {
+                LeanTween.rotate(door, rotateOpen, 1f);
+                isOpen = !isOpen;
+            }
+            
+        }
+    }
+}
