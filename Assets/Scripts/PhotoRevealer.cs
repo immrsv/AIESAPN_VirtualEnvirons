a799a3188bfc510e8ac72ac597ed9a6ea1a350c2
Riley <riley5694@gmail.com>
2017-11-14 14:49:11 +1030

    buttons change colour

--- Assets/Scripts/PhotoRevealer.cs
+++ Assets/Scripts/PhotoRevealer.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class PhotoRevealer : MonoBehaviour {
     public GameObject photo;
-	// Use this for initialization
-	void Start () {
-		
+    public GameObject UIPrompt;
+    private SteamVR_TrackedObject trackedObj;
+    private Renderer rend;
+    private SteamVR_Controller.Device Controller
+    {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+    // Use this for initialization
+    void Start () {
+        rend = GetComponent<Renderer>();
+        rend.material.color = Color.red;
 	}
 	
 	// Update is called once per frame
@@ -14,11 +22,34 @@ public class PhotoRevealer : MonoBehaviour {
 
 	}
 
-    void OnTriggerStay(Collider other)
+    void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Controller")
         {
+            UIPrompt.SetActive(true);
+            
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Controller")
+        {
+            UIPrompt.SetActive(false);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Controller" && Controller.GetPressDown(SteamVR_Controller.ButtonMask.Grip) && !photo.activeSelf)
+        {
+            rend.material.color = Color.green;
             photo.SetActive(true);
+        }
+        if (other.tag == "Controller" && Controller.GetPressDown(SteamVR_Controller.ButtonMask.Grip) && photo.activeSelf)
+        {
+            rend.material.color = Color.red;
+            photo.SetActive(false);
         }
     }
 }
