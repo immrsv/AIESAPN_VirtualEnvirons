15afc37ea624ceed46a782334789f0c1c3ca5cbb
SAPNKristian <bryanthesnowman@hotmail.com>
2017-11-14 11:52:12 +1030

    KVR door and node testing

--- VR Base Test Project/Assets/Scripts/DoorOpener.cs
+++ VR Base Test Project/Assets/Scripts/DoorOpener.cs
@@ -17,15 +17,19 @@ public class DoorOpener : MonoBehaviour {
 
     void OnTriggerStay(Collider other)
     {
+        print("door collided");
         if (other.tag == "Controller" && Controller.GetPressDown(SteamVR_Controller.ButtonMask.Grip))
         {
+            print("door grip pressed");
             if (isOpen == true)
             {
+                print("closing door");
                 LeanTween.rotate(door, rotateClose, 1f);
                 isOpen = !isOpen;
             }
             else if (isOpen == false)
             {
+                print("opening door");
                 LeanTween.rotate(door, rotateOpen, 1f);
                 isOpen = !isOpen;
             }
---
+++ VR Base Test Project/Assets/Scripts/NodeTeleporting.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeTeleporting : MonoBehaviour {
+    
+    public Transform player, node;
+
+    private SteamVR_TrackedObject trackedObj;
+    private SteamVR_Controller.Device Controller 
+    {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+
+    void OnTriggerStay (Collider other)
+    {
+        print("node collided");
+        if (other.tag == "Controller" && Controller.GetPressDown(SteamVR_Controller.ButtonMask.Grip))
+        {
+            print("node grip pressed");
+            player.transform.position = node.transform.position;
+        }
+    }
+}
