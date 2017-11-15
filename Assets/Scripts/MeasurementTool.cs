8b342574e2e5b0f6d2553d42b20e18569749126c
Riley <riley5694@gmail.com>
2017-11-15 14:22:35 +1030

    Functional MeasurementTool.

--- Assets/Scripts/MeasurementTool.cs
+++ Assets/Scripts/MeasurementTool.cs
@@ -8,6 +8,7 @@ public class MeasurementTool : MonoBehaviour {
     public GameObject endSphere;
     public LineRenderer line;
     public float distance;
+    private int sphereIndex;
 
     public TextMeshPro distanceText;
     public GameObject laserPrefab;
@@ -21,20 +22,51 @@ public class MeasurementTool : MonoBehaviour {
     public Vector3 teleportReticleOffset;
     public LayerMask teleportMask;
 
-    public SteamVR_TrackedObject trackedObj;
+
+    protected bool IsVisible = false;
+
+    public SteamVR_TrackedController trackedController;
     private SteamVR_Controller.Device Controller
     {
-        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+        get { return SteamVR_Controller.Input((int)trackedController.controllerIndex); }
     }
 
     void Awake()
     {
+        trackedController.Gripped += TrackedController_Gripped;
+        trackedController.Ungripped += TrackedController_Ungripped;
+    }
+
+    private void TrackedController_Ungripped(object sender, ClickedEventArgs e)
+    {
+        IsVisible = false;
+
+        //Debug.Log("Trigger realsed");
+        //move spheres
+        if (sphereIndex == 0)
+        {
+            //placing start
+            startSphere.transform.position = teleportReticleTransform.position - teleportReticleOffset;
+            sphereIndex = 1;
+        }
+
+        else
+        {
+            //placing end
+            endSphere.transform.position = teleportReticleTransform.position - teleportReticleOffset;
+            sphereIndex = 0;
+        }
+    }
+
+    private void TrackedController_Gripped(object sender, ClickedEventArgs e)
+    {
+        IsVisible = true; 
     }
 
     private void ShowLaser(RaycastHit hit)
     {
         laser.SetActive(true);
-        laserTransform.position = Vector3.Lerp(trackedObj.transform.position, hitPoint, .5f);
+        laserTransform.position = Vector3.Lerp(trackedController.transform.position, hitPoint, .5f);
         laserTransform.LookAt(hitPoint);
         laserTransform.localScale = new Vector3(laserTransform.localScale.x, laserTransform.localScale.y,
             hit.distance);
@@ -46,7 +78,7 @@ public class MeasurementTool : MonoBehaviour {
         var posns = new Vector3[lr.positionCount];
         var posnCount = lr.GetPositions(posns);
 
-        posns[0] = trackedObj.transform.position;
+        posns[0] = trackedController.transform.position;
         posns[1] = hit.point;
 
         lr.SetPositions(posns);
@@ -59,10 +91,39 @@ public class MeasurementTool : MonoBehaviour {
         reticle = Instantiate(teleportReticlePrefab);
         teleportReticleTransform = reticle.transform;
     }
-	
+
+    float delay = 0.5f;
+
 	// Update is called once per frame
 	void Update () {
-		if(startSphere.activeSelf && endSphere.activeSelf)
+        if ( delay > 0 ) { delay -= Time.deltaTime; return; }
+
+        if (IsVisible)
+        {
+
+            //Debug.Log("holding trigger");
+            RaycastHit hit;
+            Debug.Log(trackedController.transform.position + "  " + transform.forward);
+            if (Physics.Raycast(trackedController.transform.position, trackedController.transform.forward, out hit, 100, teleportMask))
+            {
+                Debug.Log("If is triggering");
+                hitPoint = hit.point;
+                ShowLaser(hit);
+                reticle.SetActive(true);
+
+                teleportReticleTransform.position = hitPoint + teleportReticleOffset;
+
+            }
+
+        }
+        else
+        {
+            laser.SetActive(false);
+            reticle.SetActive(false);
+        }
+
+
+        if (startSphere.activeSelf && endSphere.activeSelf)
         {
             line.SetPosition(0, startSphere.transform.localPosition);
             line.SetPosition(1, endSphere.transform.localPosition);
@@ -74,29 +135,6 @@ public class MeasurementTool : MonoBehaviour {
             distanceText.transform.position = textPos + new Vector3(0, 2, 0);
         }
 
-        if (Controller.GetPress(SteamVR_Controller.ButtonMask.Grip))
-        {
-            RaycastHit hit;
-
-            if (Physics.Raycast(trackedObj.transform.position, transform.forward, out hit, 100, teleportMask))
-            {
-                hitPoint = hit.point;
-                ShowLaser(hit);
-                reticle.SetActive(true);
-
-                teleportReticleTransform.position = hitPoint + teleportReticleOffset;
-
-            }
-        }
-        else
-        {
-            laser.SetActive(false);
-            reticle.SetActive(false);
-        }
-
-        if (Controller.GetPressUp(SteamVR_Controller.ButtonMask.Grip))
-        {
-            //move spheres
-        }
+        
     }
 }
