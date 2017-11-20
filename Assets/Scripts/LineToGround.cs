b08a471b52abbd350c4226ccd75d02fd43347ccb
Riley <riley5694@gmail.com>
2017-11-20 13:26:51 +1030

    Line render to ground

---
+++ Assets/Scripts/LineToGround.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineToGround : MonoBehaviour {
+    public LineRenderer lineToGround;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        lineToGround.SetPosition(0, gameObject.transform.localPosition);
+        lineToGround.SetPosition(1, new Vector3(transform.localPosition.x, 0, transform.localPosition.z));
+    }
+}
