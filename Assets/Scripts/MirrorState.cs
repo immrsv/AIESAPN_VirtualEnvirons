bede6009fd3378d1cc653216793531d121bcb59b
Riley <riley5694@gmail.com>
2017-11-27 12:10:39 +1030

    Door tutorial and update

---
+++ Assets/Scripts/MirrorState.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrorState : MonoBehaviour {
+    public GameObject[] targets;
+	// Use this for initialization
+	void OnEnable () {
+		foreach (var target in targets) {
+            target.SetActive(true);
+        }
+	}
+
+    void OnDisable() {
+        foreach (var target in targets) {
+            target.SetActive(false);
+        }
+    }
+}
