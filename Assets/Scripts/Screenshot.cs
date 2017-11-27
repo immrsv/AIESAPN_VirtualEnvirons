13f8026f4b9c67ab4f5373e08e9872e6eb52aa32
Riley <riley5694@gmail.com>
2017-11-27 13:11:20 +1030

    ScreenGrabber

---
+++ Assets/Scripts/Screenshot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Screenshot : MonoBehaviour {
+    protected SteamVR_TrackedController trackedObj;
+    
+    private SteamVR_Controller.Device Controller
+    {
+        get { return SteamVR_Controller.Input((int)trackedObj.controllerIndex); }
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Controller.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            ScreenCapture.CaptureScreenshot("Screenshot.png");
+        }
+	}
+}
