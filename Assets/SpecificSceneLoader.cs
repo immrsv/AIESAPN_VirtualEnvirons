d454f20fd2c993fb2daac8cc01aaf46e61d4b4c1
Riley <riley5694@gmail.com>
2017-11-27 15:26:27 +1030

    Hub Scene and Scene Loader functional.  Added menu transition between tutorial and Hub (and discovered) scenes.

--- Assets/SpecificSceneLoader.cs
+++ Assets/SpecificSceneLoader.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class SpecificSceneLoader : MonoBehaviour {
 
     public string Scene;
+    public bool LoadOnEnable = false;
+
+    private void OnEnable() {
+        if (LoadOnEnable)
+            LoadScene();
+    }
 
     void LoadScene() {
 
