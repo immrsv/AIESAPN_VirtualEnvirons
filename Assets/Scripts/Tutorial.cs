f7f762ef6e314e97c54e96156d6ad8602c38f75b
Riley <riley5694@gmail.com>
2017-12-05 10:07:45 +1030

    tutorial works now, construct measuring tool is disabled

--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -10,9 +10,10 @@ public class Tutorial : MonoBehaviour {
     public GameObject Door;
     public GameObject RedOrb;
     public GameObject GreenOrb;
+    public GameObject Photo;
 
     private Vector3 DoorLocation;
-    private int State = 0;
+    private int State = 1;
 
     public SteamVR_TrackedController trackedController;
     private SteamVR_Controller.Device Controller
@@ -22,7 +23,7 @@ public class Tutorial : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-        DoorLocation = Door.transform.position;
+        DoorLocation = Door.transform.rotation.eulerAngles;
 	}
 	
 	// Update is called once per frame
@@ -30,24 +31,39 @@ public class Tutorial : MonoBehaviour {
 
         switch (State) {
             case 1:
-                if (Controller.GetPressUp(SteamVR_Controller.ButtonMask.Touchpad))
+                if (Controller.GetPressUp(SteamVR_Controller.ButtonMask.Touchpad)) {
                     TuteText.text = "Good Job! Now hold in the Menu button to bring up the menu";
+                    State = 2;
+                }
+                    
                 break;
             case 2:
                 if (Controller.GetPressDown(SteamVR_Controller.ButtonMask.ApplicationMenu))
                     TuteText.text = "With the menu open select 'Measurement tool' and then select 'turn on'";
-                else if (MeasuringTool.activeSelf)
+                else if (MeasuringTool.activeSelf) {
                     TuteText.text = "Use the grip button to place two measuring points";
+                    State = 3;
+                }
+                    
                 break;
             case 3:
-                if (GreenOrb.activeSelf && RedOrb.activeSelf)
+                if (GreenOrb.activeSelf && RedOrb.activeSelf) {
                     TuteText.text = "You can also move the points by going up to them and picking them up with a trigger. \n\n Next go to the door and open it";
+                    State = 4;
+                }
+                    
                 break;
             case 4:
-                if (Door.transform.position != DoorLocation)
-                    TuteText.text = "That is all of the base functionality! Open the menu and select a different scene to load it up to complete the tutorial";
+                if (Door.transform.rotation.eulerAngles != DoorLocation) {
+                    TuteText.text = "Go to the sign and turn on the photo";
+                    State = 5;
+                }
                 break;
-
+            case 5:
+                if(Photo.activeSelf == true) {
+                    TuteText.text = "That is all of the base functionality! Open the menu and select Scene Hub to complete the tutorial";
+                }
+                break;
         }
 	}
 }
